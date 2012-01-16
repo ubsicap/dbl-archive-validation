4e24521334a9384e810c56411d58793b2b7026d9
jklassen <none@none>
2012-01-16 14:09:05 -0500

    Version 9 of schema - see top notes in usx.rnc

--- converttorng.bat
+++ converttorng.bat
@@ -1,2 +1,3 @@
 java -jar trang.jar -I rnc -O rng usx.rnc usx.rng
 java -jar trang.jar -I rnc -O rng metadata_project.rnc metadata_project.rng
+java -jar trang.jar -I rnc -O rng usx_styles.rnc usx_styles.rng
