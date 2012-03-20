1585f5eda4e35bb16700b3f10c96f6427b0f392b
Clayton <none@none>
2012-03-20 15:19:42 -0400

    Minor audio fixes

--- converttorng.bat
+++ converttorng.bat
@@ -2,3 +2,5 @@ java -jar trang.jar -I rnc -O rng source/text/1.2/usx.rnc source/text/1.2/usx.rn
 java -jar trang.jar -I rnc -O rng source/text/1.2/metadata.rnc source/text/1.2/metadata.rng
 java -jar trang.jar -I rnc -O rng source/text/1.2/styles.rnc source/text/1.2/styles.rng
 java -jar trang.jar -I rnc -O rng source/text/1.2/license.rnc source/text/1.2/license.rng
+java -jar trang.jar -I rnc -O rng source/audio/1.0/license.rnc source/audio/1.0/license.rng
+java -jar trang.jar -I rnc -O rng source/audio/1.0/metadata.rnc source/audio/1.0/metadata.rng
