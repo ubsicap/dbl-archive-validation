b2128e098aa141b362a57d02b55722ef80319d08
ericpyle <eric_pyle@sil.org>
2016-06-15 12:30:15 -0500

    begin changing source for worlist lemma char

--- converttorng.bat
+++ converttorng.bat
@@ -1,19 +1,8 @@
-java -jar trang.jar -I rnc -O rng source/text/1.4/usx.rnc source/text/1.4/usx.rng
-java -jar trang.jar -I rnc -O rng source/text/1.4/metadata.rnc source/text/1.4/metadata.rng
-java -jar trang.jar -I rnc -O rng source/text/1.4/styles.rnc source/text/1.4/styles.rng
-java -jar trang.jar -I rnc -O rng source/text/1.4/license.rnc source/text/1.4/license.rng
-java -jar trang.jar -I rnc -O rng source/text/1.3/usx.rnc source/text/1.3/usx.rng
-java -jar trang.jar -I rnc -O rng source/text/1.3/metadata.rnc source/text/1.3/metadata.rng
-java -jar trang.jar -I rnc -O rng source/text/1.3/styles.rnc source/text/1.3/styles.rng
-java -jar trang.jar -I rnc -O rng source/text/1.3/license.rnc source/text/1.3/license.rng
-java -jar trang.jar -I rnc -O rng source/text/1.2/usx.rnc source/text/1.2/usx.rng
-java -jar trang.jar -I rnc -O rng source/text/1.2/metadata.rnc source/text/1.2/metadata.rng
-java -jar trang.jar -I rnc -O rng source/text/1.2/styles.rnc source/text/1.2/styles.rng
-java -jar trang.jar -I rnc -O rng source/text/1.2/license.rnc source/text/1.2/license.rng
+java -jar trang.jar -I rnc -O rng latest/dev/usx.rnc latest/dev/usx.rng
+java -jar trang.jar -I rnc -O rng latest/prod/text/usx.rnc latest/prod/text/usx.rng
+java -jar trang.jar -I rnc -O rng source/text/1.5/usx.rnc source/text/1.5/usx.rng
+java -jar trang.jar -I rnc -O rng source/text/1.5/metadata.rnc source/text/1.5/metadata.rng
+java -jar trang.jar -I rnc -O rng source/text/1.5/styles.rnc source/text/1.5/styles.rng
 
 java -jar trang.jar -I rnc -O rng source/audio/1.2/license.rnc source/audio/1.2/license.rng
 java -jar trang.jar -I rnc -O rng source/audio/1.2/metadata.rnc source/audio/1.2/metadata.rng
-java -jar trang.jar -I rnc -O rng source/audio/1.1/license.rnc source/audio/1.1/license.rng
-java -jar trang.jar -I rnc -O rng source/audio/1.1/metadata.rnc source/audio/1.1/metadata.rng
-java -jar trang.jar -I rnc -O rng source/audio/1.0/license.rnc source/audio/1.0/license.rng
-java -jar trang.jar -I rnc -O rng source/audio/1.0/metadata.rnc source/audio/1.0/metadata.rng
