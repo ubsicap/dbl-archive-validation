98cb68af28931c94cc66e7024e5e45ce10f74701
Mark Howe <mvahowe@gmail.com>
2017-04-13 17:41:09 +0200

    Versioned source fragments; new directory structure

--- v2/2_0/src/make.py
+++ v2/2_0/src/make.py
@@ -13,15 +13,21 @@ outputFilename = sys.argv[2]
 fileContents = ""
 directory = os.path.dirname(os.path.realpath(__file__))
 
+inserted = {}
 subRE = re.compile(r"%%insert ([A-Za-z0-9_.-]+)%%")
 with open(inputFilename, 'r') as inputFileHandle:
    fileContents = inputFileHandle.read()
    match = re.search(subRE, fileContents)
    while match:
-	   insertFilename = match.group(1)
-	   print "   " + "inserting " + insertFilename
-	   fileContents = re.sub(match.group(0), open(os.path.join(directory, insertFilename), "r").read(), fileContents)
-	   match = re.search(subRE, fileContents)
+      insertFilename = match.group(1)
+      if insertFilename in inserted:
+         print "   " + "skipping " + insertFilename + " (already inserted)"
+         fileContents = re.sub(match.group(0), "", fileContents, count=1)
+      else:
+         inserted[insertFilename] = True
+         print "   " + "inserting " + insertFilename
+         fileContents = re.sub(match.group(0), open(os.path.join(directory, insertFilename), "r").read(), fileContents, count=1)
+      match = re.search(subRE, fileContents)
 
 with open(outputFilename, 'w') as outputFileHandle:
    outputFileHandle.write(fileContents)
