e8072bae245b5d21c86e88987484584d68cd81ae
Mark Howe <mvahowe@gmail.com>
2018-02-08 16:03:00 +0100

    Validate generated RNG as part of make process

--- v2/tools/make.sh
+++ v2/tools/make.sh
@@ -20,17 +20,22 @@ echo "   job-spec-upload.rnc"
 echo "   dbl-xhtml.rnc"
 ./makeRNC.py --quiet $srcDir/src/xhtml_standalone_rnc_$version.template $srcDir/schemas/dbl-xhtml.rnc
 
-echo "Make RNG"
+echo "Make RNG and validate"
 echo "   metadata.rng"
 java -jar ./trang.jar -I rnc -O rng $srcDir/schemas/metadata.rnc $srcDir/schemas/metadata.rng
+xmllint --relaxng relaxng.rng -noout $srcDir/schemas/metadata.rng 2> /dev/null
 echo "   metadata-upload.rng"
 java -jar ./trang.jar -I rnc -O rng $srcDir/schemas/metadata-upload.rnc $srcDir/schemas/metadata-upload.rng
+xmllint --relaxng relaxng.rng -noout $srcDir/schemas/metadata-upload.rng 2> /dev/null
 echo "   metadata-template.rng"
 java -jar ./trang.jar -I rnc -O rng $srcDir/schemas/metadata-template.rnc $srcDir/schemas/metadata-template.rng
+xmllint --relaxng relaxng.rng -noout $srcDir/schemas/metadata-template.rng 2> /dev/null
 echo "   job-spec-upload.rnc"
 java -jar ./trang.jar -I rnc -O rng $srcDir/schemas/job-spec-upload.rnc $srcDir/schemas/job-spec-upload.rng
+xmllint --relaxng relaxng.rng -noout $srcDir/schemas/job-spec-upload.rng 2> /dev/null
 echo "   dbl-xhtml.rnc"
 java -jar ./trang.jar -I rnc -O rng $srcDir/schemas/dbl-xhtml.rnc $srcDir/schemas/dbl-xhtml.rng
+xmllint --relaxng relaxng.rng -noout $srcDir/schemas/dbl-xhtml.rng 2> /dev/null
 
 echo "Make XSD"
 echo "   dbl-xhtml.xsd"
