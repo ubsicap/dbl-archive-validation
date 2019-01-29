b476c7cf3a0bfb97ea08f4d4314b6536a76b7ab9
Mark Howe <mvahowe@gmail.com>
2019-01-29 12:26:36 +0100

    Start of MetadataAsJson

---
+++ v2/to_json/metadata_to_json.py
@@ -0,0 +1,54 @@
+import json
+from lxml import etree
+
+from .metadata_as_json_exception import MetadataAsJsonException
+
+
+class MetadataAsJson():
+    """
+    Class for converting between XML and JSON expressions of DBL Metadata.
+    """
+    
+    xml_dom = None
+    json_dict = None
+    
+    def __init__(self, xml_dom=None, xml_doc=None, json_dict=None, json_doc=None):
+
+        def _init_from_dom():
+            self.xml_dom = xml_dom
+            self.json_dict = self.dict_from_dom()
+
+        def _init_from_dict():
+            self.json_dict = json_dict
+            self.xml_dom = self.dom_from_dict()
+
+        # Require exactly one data source
+        sources = [s for s in [xml_dom, xml_doc, json_dict, json_doc] if s is not None]
+        if len(sources) != 1:
+            raise MetadataAsJsonException(
+                {
+                    "code": "ARGS-001",
+                    "printable": "MetadataAsJson constructor requires exactly one data source"
+                }
+            )
+        # Convert docs to data structures, then dispatch to source-specific helper functions
+        if xml_doc is not None:
+            xml_dom = etree.fromstring(xml_doc)
+        if xml_dom is not None:
+            _init_from_dom()
+        else:
+            if json_doc is not None:
+                json_dict = json.loads(json_doc)
+            _init_from_dict()
+
+    def dict_from_dom(self):
+        """
+        Return a dictionary based on xml_dom
+        """
+        return {}
+
+    def dom_from_dict(self):
+        """
+        Return dom based on json_dict
+        """
+        return etree.Element("DBLMetadata")
---
+++ v2/to_json/test/test_init.py
@@ -0,0 +1,77 @@
+import json
+import os
+import pytest
+import re
+
+
+from lxml import etree
+
+
+from ..metadata_to_json import MetadataAsJson
+from ..metadata_as_json_exception import MetadataAsJsonException
+
+def _no_exception_surprises(exc, code, printable_regex):
+    assert _exception_surprises(exc, code, printable_regex) is None
+
+def _exception_surprises(exc, code, printable_regex):
+    """
+    Returns None if the exception has the correct code and matches the regex.
+    In other cases it returns a diagnostic string.
+    """
+    exception_dict = exc.value.args[0]
+    if code != exception_dict["code"]:
+        return "Expected code {0}, found {1}".format(code, exception_dict["code"])
+    if not re.search(printable_regex, exception_dict["printable"]):
+        return "No match for '{0}' in '{1}'".format(printable_regex, exception_dict["printable"])
+
+def _test_data_dir():
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), "test_data"))
+
+def test_no_source_fail():
+    """
+    Init should fail if no source provided.
+    """
+    with pytest.raises(MetadataAsJsonException) as exc:
+        MetadataAsJson()
+    _no_exception_surprises(exc, "ARGS-001", "exactly one")
+
+def test_multiple_source_fail():
+    """
+    Init should fail if multiple sources provided.
+    """
+    with pytest.raises(MetadataAsJsonException) as exc:
+        MetadataAsJson(xml_doc="<foo/>", xml_dom=etree.fromstring("<foo/>"))
+    _no_exception_surprises(exc, "ARGS-001", "exactly one")
+    with pytest.raises(MetadataAsJsonException) as exc:
+        MetadataAsJson(json_doc="{'foo': 'baa'}", json_dict={"foo": "baa"})
+    _no_exception_surprises(exc, "ARGS-001", "exactly one")
+
+@pytest.mark.parametrize(
+    'source',
+    ["text.xml", "audio.xml", "video.xml", "print.xml", "braille.xml"]
+)
+def test_template_xml_init(source):
+    """
+    Init should work with DBL.Local XML templates (which are not valid and extremely incomplete).
+    """
+    fq_source = os.path.join(_test_data_dir(), "xml", "template_metadata", source)
+    with open(fq_source, "rb") as xml_in:
+        doc = xml_in.read()
+    maj = MetadataAsJson(xml_doc=doc)
+    dom = etree.fromstring(doc)
+    maj = MetadataAsJson(xml_dom=dom)
+
+@pytest.mark.parametrize(
+    'source',
+    ["stub.json"]
+)
+def test_template_json_init(source):
+    """
+    Init should work with the JSON expression of DBL.Local XML templates (which are not valid and extremely incomplete).
+    """
+    fq_source = os.path.join(_test_data_dir(), "json", "template_metadata", source)
+    with open(fq_source, "rb") as json_in:
+        doc = json_in.read()
+    maj = MetadataAsJson(json_doc=doc)
+    json_dict = json.loads(doc)
+    maj = MetadataAsJson(json_dict=json_dict)
